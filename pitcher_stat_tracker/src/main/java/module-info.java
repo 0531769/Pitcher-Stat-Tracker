d74522a5c8a213b28b5dcc64d1a34727e4efda71
0531769 <142849780+0531769@users.noreply.github.com>
2024-04-22 20:10:44 -0400

    Added the JavaFX files to the repo

---
+++ pitcher_stat_tracker/src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module pitcher_project_team.pitcher_stat_tracker {
+    requires javafx.controls;
+    exports pitcher_project_team.pitcher_stat_tracker;
+}
---
+++ pitcher_stat_tracker/src/main/java/pitcher_project_team/pitcher_stat_tracker/SystemInfo.java
@@ -0,0 +1,13 @@
+package pitcher_project_team.pitcher_stat_tracker;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
