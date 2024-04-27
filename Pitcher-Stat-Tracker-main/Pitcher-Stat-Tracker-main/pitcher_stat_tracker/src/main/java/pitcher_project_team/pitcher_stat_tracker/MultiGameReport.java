28f497fa1280951a5e29d61c5407811dcbb0b728
0288616 <142856213+0288616@users.noreply.github.com>
2024-04-27 19:08:29 -0400

    Update MultiGameReport.java

--- Pitcher-Stat-Tracker-main/Pitcher-Stat-Tracker-main/pitcher_stat_tracker/src/main/java/pitcher_project_team/pitcher_stat_tracker/MultiGameReport.java
+++ Pitcher-Stat-Tracker-main/Pitcher-Stat-Tracker-main/pitcher_stat_tracker/src/main/java/pitcher_project_team/pitcher_stat_tracker/MultiGameReport.java
@@ -1,4 +1,10 @@
-package com.mycompany.finalproject;
+/*
+ * Name: Julius Peterson
+ * Date: 4/27/24
+ * Team: Pitcher Team (Trevor Pence, Julius Peterson, Jay Lee)
+ * Purpose: Generate a report for multiple games.
+ */
+package pitcher_project_team.pitcher_stat_tracker;
 
 import java.io.FileWriter;
 import java.io.IOException;
@@ -7,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class MultiGameReport {
+
     private List<String> gameDates;
     private String outputFilePath;
 
@@ -15,57 +22,66 @@ public class MultiGameReport {
         this.outputFilePath = outputFilePath;
     }
 
-    public void generateReport() {
-        try (Connection connection = DriverManager.getConnection("jdbc:sqlite:pitcher_stats.sqlite")) {
-            List<String> reportLines = new ArrayList<>();
-            reportLines.add("Multi-Game Summary Page:");
-            reportLines.add("");
-            reportLines.add("Game Date\tFirst Name\tLast Name\tTeam Name\tInnings Pitched\tHits\tRuns\t" +
-                    "Earned Runs\tWalks\tStrikeouts\tAt Bats\tBatters Faced\tNumber of Pitches");
+   public void generateTotalStatisticsReport() {
+    try (Connection connection = DriverManager.getConnection("jdbc:sqlite:pitcher_stats.sqlite")) {
+        List<String> reportLines = new ArrayList<>();
+        reportLines.add("Total Statistics Summary:");
+        reportLines.add("");
+        reportLines.add("Player\tInnings Pitched\tHits\tRuns\tEarned Runs\tWalks\tStrikeouts\tAt Bats\tBatters Faced\tNumber of Pitches");
 
-            String selectSQL = "SELECT * FROM PitcherStats WHERE DateOfGame IN (";
-            for (int i = 0; i < gameDates.size(); i++) {
-                if (i > 0) {
-                    selectSQL += ",";
-                }
-                selectSQL += "?";
+        String selectSQL = "SELECT FirstName, LastName, " +
+                "SUM(InningsPitched) AS TotalInningsPitched, " +
+                "SUM(Hits) AS TotalHits, " +
+                "SUM(Runs) AS TotalRuns, " +
+                "SUM(EarnedRuns) AS TotalEarnedRuns, " +
+                "SUM(Walks) AS TotalWalks, " +
+                "SUM(Strikeouts) AS TotalStrikeouts, " +
+                "SUM(AtBats) AS TotalAtBats, " +
+                "SUM(BattersFaced) AS TotalBattersFaced, " +
+                "SUM(NumberOfPitches) AS TotalNumberOfPitches " +
+                "FROM PitcherStats WHERE DateOfGame IN (";
+
+        for (int i = 0; i < gameDates.size(); i++) {
+            if (i > 0) {
+                selectSQL += ",";
             }
-            selectSQL += ")";
-            
-            try (PreparedStatement ps = connection.prepareStatement(selectSQL)) {
-                for (int i = 0; i < gameDates.size(); i++) {
-                    ps.setString(i + 1, gameDates.get(i));
-                }
-                
-                try (ResultSet rs = ps.executeQuery()) {
-                    while (rs.next()) {
-                        String gameDate = rs.getString("DateOfGame");
-                        String firstName = rs.getString("FirstName");
-                        String lastName = rs.getString("LastName");
-                        String teamName = rs.getString("TeamName");
-                        double inningsPitched = rs.getDouble("InningsPitched");
-                        int hits = rs.getInt("Hits");
-                        int runs = rs.getInt("Runs");
-                        int earnedRuns = rs.getInt("EarnedRuns");
-                        int walks = rs.getInt("Walks");
-                        int strikeouts = rs.getInt("Strikeouts");
-                        int atBats = rs.getInt("AtBats");
-                        int battersFaced = rs.getInt("BattersFaced");
-                        int numberOfPitches = rs.getInt("NumberOfPitches");
-
-                        String line = String.format("%s\t%s\t%s\t%.1f\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d",
-                                gameDate, firstName, lastName, teamName, inningsPitched, hits, runs, earnedRuns,
-                                walks, strikeouts, atBats, battersFaced, numberOfPitches);
-                        reportLines.add(line);
-                    }
-                }
-            }
-
-            writeReportToFile(reportLines);
-        } catch (SQLException | IOException e) {
-            e.printStackTrace();
+            selectSQL += "?";
         }
+        selectSQL += ") GROUP BY FirstName, LastName";
+        
+        try (PreparedStatement ps = connection.prepareStatement(selectSQL)) {
+            for (int i = 0; i < gameDates.size(); i++) {
+                ps.setString(i + 1, gameDates.get(i));
+            }
+            
+            try (ResultSet rs = ps.executeQuery()) {
+                while (rs.next()) {
+                    String firstName = rs.getString("FirstName");
+                    String lastName = rs.getString("LastName");
+                    double inningsPitched = rs.getDouble("TotalInningsPitched");
+                    int hits = rs.getInt("TotalHits");
+                    int runs = rs.getInt("TotalRuns");
+                    int earnedRuns = rs.getInt("TotalEarnedRuns");
+                    int walks = rs.getInt("TotalWalks");
+                    int strikeouts = rs.getInt("TotalStrikeouts");
+                    int atBats = rs.getInt("TotalAtBats");
+                    int battersFaced = rs.getInt("TotalBattersFaced");
+                    int numberOfPitches = rs.getInt("TotalNumberOfPitches");
+
+                    String line = String.format("%s %s\t%.1f\t%d\t%d\t%d\t%d\t%d\t%d\t%d\t%d",
+                            firstName, lastName, inningsPitched, hits, runs, earnedRuns,
+                            walks, strikeouts, atBats, battersFaced, numberOfPitches);
+                    reportLines.add(line);
+                }
+            }
+        }
+
+        writeReportToFile(reportLines);
+    } catch (SQLException | IOException e) {
+        e.printStackTrace();
     }
+}
+
 
     private void writeReportToFile(List<String> reportLines) throws IOException {
         try (FileWriter writer = new FileWriter(outputFilePath)) {
@@ -75,3 +91,4 @@ public class MultiGameReport {
         }
     }
 }
+
